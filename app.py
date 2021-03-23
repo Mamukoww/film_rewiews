b7cc6c606bb3979829266e517e168b3af38b08ec
Lavrenty Mamukow <mamukoww04@yandex.ru>
2021-03-23 20:31:19 +0300

    Add 404 page for films

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from films import films, find_by_name
 
 app = Flask(__name__)
@@ -11,6 +11,8 @@ def homepage():
 
 @app.route("/films/<int:film_id>")
 def get_film(film_id):
+    if film_id not in films:
+        abort(404)
     return render_template("film.html", film=films[film_id])
 
 
@@ -18,6 +20,11 @@ def get_film(film_id):
 def search():
     name = request.args.get("name", "")
     return render_template('index.html', title="FilmReviews", films=find_by_name(name))
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("errors/404.html"), 404
 
 
 if __name__ == '__main__':
