f40352f8005b2d89ca9162b2568688513d232a07
Lavrenty Mamukow <mamukoww04@yandex.ru>
2021-03-23 15:23:51 +0300

    Main page with Flask

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from films import films
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html', title="FilmReviews", films=films)
+
+@app.route('/about')
+def about():
+    return "About us"
+
+@app.route("/films/<int:film_id>")
+def get_film(film_id):
+    return f"Film #{film_id}"
 
 
 if __name__ == '__main__':
---
+++ films.py
@@ -0,0 +1,17 @@
+films = {
+    1:{
+        "name": "Звёздные войны. Эпизод IV: Новая надежда",
+        "release_date": "25 мая 1977 года",
+        "image": "https://upload.wikimedia.org/wikipedia/ru/8/87/StarWarsMoviePoster1977.jpg"
+    },
+    2:{
+        "name": "Назад в будущее",
+        "release_date": "3 июля 1985 года",
+        "image": "https://upload.wikimedia.org/wikipedia/ru/9/90/BTTF_DVD_rus.jpg"
+    },
+    3:{
+            "name": "ВАЛЛ-И",
+            "release_date": "27 июня 2008 года",
+            "image": "https://upload.wikimedia.org/wikipedia/ru/thumb/c/c4/WALL-E_poster.png/266px-WALL-E_poster.png"
+        }
+}
