06eeaf67097a436199e94f5939ad418b29f1f76a
Lavrenty Mamukow <mamukoww04@yandex.ru>
2021-04-03 17:03:39 +0300

    Add register logic

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from forms import FilmForm, LoginForm, RegisterForm
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, request, abort
 from models import db, User, Film, Review
+from flask import Flask, render_template, request, abort, redirect, url_for
 from flask_migrate import Migrate
 import datetime
+from flask_login import login_user, LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 app.secret_key = 'dSalGiK8IC9~fxz'
 db.init_app(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 Bootstrap(app)
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
@@ -20,13 +27,26 @@ def homepage():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            abort(400)
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        user = User(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user, remember=True)
+        return redirect(url_for('homepage'))
     return render_template('register.html', form=form)
 
 
